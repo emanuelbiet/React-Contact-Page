eb41fa54a7195bc81e5ec88fe8f18211b902bd10
Emanuel <erbiet@outlook.com>
2020-07-27 17:47:19 -0300

    feat: implement states and change layout

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,20 +1,65 @@
 import React, { useState } from "react";
 import InputMask from "react-input-mask";
+
+import api from "../../services/api";
+
 import "./index.css";
+import logoImg from "../../assets/mail.png";
+
+import { Error } from "./styles";
 
 const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [message, setMessage] = useState("");
+  const [attachedFile, setAttachedFile] = useState("");
+  const [inputError, setInputError] = useState("");
+
+  const onFormSubmit = async (e) => {
+    e.preventDefault();
+
+    await api
+      .post("/store", {
+        name,
+        email,
+        telephone,
+        message,
+        attachedFile,
+      })
+      .then(
+        (response) => {
+          console.log(response);
+          setInputError("");
+        },
+        (error) => {
+          setInputError(error.message);
+        }
+      );
+  };
 
   return (
     <>
-      <p>Página de Contato</p>
+      <div className="centeredDiv">
+        <img src={logoImg} alt="Mail"></img>
+      </div>
+
       <div>
-        <form encType="multipart/form-data">
+        <form onSubmit={onFormSubmit} encType="multipart/form-data">
+          {inputError && (
+            <Error>
+              <strong>{inputError}</strong>
+            </Error>
+          )}
+
           <label>Nome</label>
           <input
             type="text"
             id="name"
             name="name"
+            onChange={(e) => setName(e.target.value)}
             placeholder="Insira seu nome"
+            data-testid="name"
             required
           />
           <label>E-mail</label>
@@ -23,6 +68,8 @@ const Form = () => {
             id="email"
             name="email"
             placeholder="Insira seu e-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            data-testid="email"
             required
           />
           <label>Telefone</label>
@@ -32,30 +79,39 @@ const Form = () => {
             name="telephone"
             placeholder="Insira seu número de telefone"
             mask="(99) 99999-9999"
+            onChange={(e) => setTelephone(e.target.value)}
+            data-testid="telephone"
             required
           />
           <label>Mensagem</label>
-          <textarea 
-          id="message" 
-          name="message"
-          required
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Insira sua mensagem"
+            onChange={(e) => setMessage(e.target.value)}
+            data-testid="message"
+            required
           ></textarea>
-          <label class="custom-input" for="attachedFile">
+          <label className="custom-input" htmlFor="attachedFile">
             Anexar arquivo
           </label>
           <input
-          type="file"
-          id="attachedFile"
-          name="attachedFile"
-          required
+            type="file"
+            id="attachedFile"
+            name="attachedFile"
+            onChange={(e) =>
+              setAttachedFile(e.target.files || e.dataTransfer.files)
+            }
+            data-testid="attachedFile"
+            required
           />
-          <br />
-          <br />
-          <input type="submit" value="Enviar" />
+          <div className="centeredDiv">
+            <input type="submit" data-testid="submitButton" value="Enviar" />
+          </div>
         </form>
       </div>
     </>
   );
-}
+};
 
 export default Form;
---
+++ src/components/Form/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Error = styled.span`
+  margin-bottom: 24px;
+  width: auto;
+  padding: 16px;
+  display: block;
+  color: #fff;
+  font-weight: 400;
+  background-color: #c53030;
+  border-radius: 4px;
+`;
